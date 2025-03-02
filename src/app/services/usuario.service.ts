b2321ab9851a57d86652f7ff42d10693b0989992
Carlos <juancarlosgomezpacheco283@gmail.com>
2025-03-02 12:28:20 -0600

    Se hizo implementacion del sessionStorage para el logueo de ususarios

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Usuario } from '../models/usuario.model';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -9,44 +10,50 @@ import { Usuario } from '../models/usuario.model';
 export class UsuarioService {
   public apiUrl = 'http://localhost:8081/usuarios'; // Ruta base para usuarios
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   private obtenerToken(): string | null {
-    return sessionStorage.getItem('token');
+    if (isPlatformBrowser(this.platformId)) {
+      return sessionStorage.getItem('token');
+    }
+    return null;
   }
 
   // Método para obtener todos los usuarios
   obtenerTodos(): Observable<Usuario[]> {
     const token = this.obtenerToken();
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    const headers = token ? new HttpHeaders().set('Authorization', `Bearer ${token}`) : new HttpHeaders();
     return this.http.get<Usuario[]>(`${this.apiUrl}/list`, { headers });
   }
 
   // Método para obtener un usuario por su ID
   obtenerPorId(id: number): Observable<Usuario> {
     const token = this.obtenerToken();
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    const headers = token ? new HttpHeaders().set('Authorization', `Bearer ${token}`) : new HttpHeaders();
     return this.http.get<Usuario>(`${this.apiUrl}/list/${id}`, { headers });
   }
 
   // Método para crear un nuevo usuario
   crearUsuario(usuario: Usuario): Observable<Usuario> {
     const token = this.obtenerToken();
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    const headers = token ? new HttpHeaders().set('Authorization', `Bearer ${token}`) : new HttpHeaders();
     return this.http.post<Usuario>(`${this.apiUrl}/save`, usuario, { headers });
   }
 
   // Método para actualizar un usuario existente
   actualizarUsuario(id: number, usuario: Usuario): Observable<Usuario> {
     const token = this.obtenerToken();
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    const headers = token ? new HttpHeaders().set('Authorization', `Bearer ${token}`) : new HttpHeaders();
     return this.http.put<Usuario>(`${this.apiUrl}/update/${id}`, usuario, { headers });
   }
 
   // Método para eliminar un usuario
   eliminarUsuario(id: number): Observable<void> {
     const token = this.obtenerToken();
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    const headers = token ? new HttpHeaders().set('Authorization', `Bearer ${token}`) : new HttpHeaders();
     return this.http.delete<void>(`${this.apiUrl}/delete/${id}`, { headers });
   }
 
@@ -56,7 +63,6 @@ export class UsuarioService {
   }
 
   estaAutenticado(): boolean {
-    const token = sessionStorage.getItem('token');
-    return !!token; // Devuelve true si el token existe
+    return isPlatformBrowser(this.platformId) && !!sessionStorage.getItem('token');
   }
 }
