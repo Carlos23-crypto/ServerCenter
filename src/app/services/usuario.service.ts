5cd5bbff842abe2c4fced81ecbef93ecd9afb3bb
Carlos <juancarlosgomezpacheco283@gmail.com>
2025-03-02 02:19:32 -0600

    Se empezo con la creacion de las rutas, asi como el catalogo principal

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Usuario } from '../models/usuario.model';
 
@@ -7,46 +7,56 @@ import { Usuario } from '../models/usuario.model';
   providedIn: 'root'
 })
 export class UsuarioService {
-    [x: string]: any;
   public apiUrl = 'http://localhost:8081/usuarios'; // Ruta base para usuarios
 
   constructor(private http: HttpClient) { }
 
+  private obtenerToken(): string | null {
+    return sessionStorage.getItem('token');
+  }
+
   // Método para obtener todos los usuarios
   obtenerTodos(): Observable<Usuario[]> {
-    return this.http.get<Usuario[]>(`${this.apiUrl}/list`);
+    const token = this.obtenerToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<Usuario[]>(`${this.apiUrl}/list`, { headers });
   }
 
   // Método para obtener un usuario por su ID
   obtenerPorId(id: number): Observable<Usuario> {
-    return this.http.get<Usuario>(`${this.apiUrl}/list/${id}`);
+    const token = this.obtenerToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<Usuario>(`${this.apiUrl}/list/${id}`, { headers });
   }
 
   // Método para crear un nuevo usuario
   crearUsuario(usuario: Usuario): Observable<Usuario> {
-    return this.http.post<Usuario>(`${this.apiUrl}/save`, usuario);
+    const token = this.obtenerToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post<Usuario>(`${this.apiUrl}/save`, usuario, { headers });
   }
 
   // Método para actualizar un usuario existente
   actualizarUsuario(id: number, usuario: Usuario): Observable<Usuario> {
-    return this.http.put<Usuario>(`${this.apiUrl}/update/${id}`, usuario);
+    const token = this.obtenerToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.put<Usuario>(`${this.apiUrl}/update/${id}`, usuario, { headers });
   }
 
   // Método para eliminar un usuario
   eliminarUsuario(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/delete/${id}`);
+    const token = this.obtenerToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.delete<void>(`${this.apiUrl}/delete/${id}`, { headers });
   }
 
-   // Método para validar el login
-   login(nombreUsuario: string, contrasena: string): Observable<any> {
+  // Método para validar el login
+  login(nombreUsuario: string, contrasena: string): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/login`, { nombreUsuario, contrasena });
   }
 
   estaAutenticado(): boolean {
-    if (typeof sessionStorage !== 'undefined') {
-      const token = sessionStorage.getItem('token');
-      return !!token; // Devuelve true si el token existe
-    }
-    return false; // Si sessionStorage no está disponible, asume que no está autenticado
+    const token = sessionStorage.getItem('token');
+    return !!token; // Devuelve true si el token existe
   }
 }
